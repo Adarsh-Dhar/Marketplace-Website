ad53656e2d5016111984953a3a352933a51dfd9c
Adarsh Ayushman Dhar <adarsh@Adarshs-MacBook-Air.local>
2024-03-28 20:51:45 +0530

    favicon added

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -7,9 +7,12 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const customer_1 = __importDefault(require("./routes/customer")); // Import your router file
 const shop_1 = __importDefault(require("./routes/shop"));
+const favicon = require("serve-favicon");
+const path = require("path");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 // Mount the router for the /signin endpoint
 app.use("/customer", customer_1.default);
 app.use("/shop", shop_1.default);
--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import customerRouter from "./routes/customer"; // Import your router file
 import shopRouter from "./routes/shop"
+const favicon = require("serve-favicon")
+const path = require("path")
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(favicon(path.join(__dirname,"public","favicon.ico")))
+
 
 // Mount the router for the /signin endpoint
 app.use("/customer", customerRouter);
