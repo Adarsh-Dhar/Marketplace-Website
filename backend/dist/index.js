bab19b795b9fd372e0c1192ac256decf12e46bba
Adarsh Ayushman Dhar <adarsh@Adarshs-MacBook-Air.local>
2024-04-08 14:42:02 +0530

    deployment starts

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -7,23 +7,9 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const customer_1 = __importDefault(require("./routes/customer")); // Import your router file
 const shop_1 = __importDefault(require("./routes/shop"));
-const favicon = require("serve-favicon");
-const path = require("path");
-const helmet = require('helmet');
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(helmet());
-app.use(helmet({
-    contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", "https://marketplace-website-7.onrender.com"],
-            // Add other directives as necessary
-        },
-    },
-}));
 // Mount the router for the /signin endpoint
 app.use("/customer", customer_1.default);
 app.use("/shop", shop_1.default);
